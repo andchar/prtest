3aa3845a5187e61709265a2a08754cf55d12a136
andchar <63967764+andchar@users.noreply.github.com>
2023-03-12 11:59:51 -0400

    Create deploy.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,18 @@
+def call(body) {
+  def pipelineParams = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+  pipeline {
+    agent { any }
+    stages {
+      stage('Test print') {
+        steps {
+          script {
+            echo "Hello"
+          }
+        }
+      }
+    }
+  }
+  
